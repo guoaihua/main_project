86038ef5df94bde1c60bcc4c5fec9b033b6a04af
guoaihua <308232015@qq.com>
2017-08-16 20:28:24 +0800

    :construction:  update home page

--- zming/src/components/home.vue
+++ zming/src/components/home.vue
@@ -1,17 +1,50 @@
 <template>
 	<div class="home ">
 		<div class="box clear">
-			<img src="/static/imgs/2.jpg" class='img-responsive'>
+			<img src="/static/imgs/borders1.png" class='img-responsive'>
+			<marquee behavior="scroll" direction="up" class="scrollText">
+				<p>生命里，一些缱绻，无论素净，还是喧哗，都已经被岁月赋予了清喜的味道，一些闲词，或清新，或淡雅，总会在某一个回眸的时刻醉了流年，濡湿了柔软的心，冥冥之中，我们沿着呼唤的风声，终于在堆满落花的秋里，再次重逢，念在天涯，心在咫尺，我相信，一米阳光，才是我们最好的距离。   
+       缘起是诗，缘离是画，这些关于岁月，关于记忆的章节，终会被时光搁置在无法触及的红尘之外，曾经，你我一别经年，可风里，总有一段美丽会与我们不期而遇，一盏琉璃，半杯心悦，端然着那一份醉人的静，这安静行走的流年，总会被过往赋予一份清喜，一份浪漫。</p>
+       				<p>生命里，一些缱绻，无论素净，还是喧哗，都已经被岁月赋予了清喜的味道，一些闲词，或清新，或淡雅，总会在某一个回眸的时刻醉了流年，濡湿了柔软的心，冥冥之中，我们沿着呼唤的风声，终于在堆满落花的秋里，再次重逢，念在天涯，心在咫尺，我相信，一米阳光，才是我们最好的距离。   
+       缘起是诗，缘离是画，这些关于岁月，关于记忆的章节，终会被时光搁置在无法触及的红尘之外，曾经，你我一别经年，可风里，总有一段美丽会与我们不期而遇，一盏琉璃，半杯心悦，端然着那一份醉人的静，这安静行走的流年，总会被过往赋予一份清喜，一份浪漫。</p>
+			</marquee>
 			<div class="intro">
 				<div class="line">
-					<h3>Hello</h3>
-					<h3>I'm GAH</h3>
-					<h6>full-stack</h6>
+					<h3>你好！ 我叫郭爱华</h3>
+					<div class="rw-words">
+						<span>励志成为webfull-stack</span>
+						<span>励志成为webfull-stack</span>
+						<span>励志成为webfull-stack</span>
+						<span>励志成为webfull-stack</span>
+						<span>励志成为webfull-stack</span>
+						<span>励志成为webfull-stack</span>
+					</div>					
+					<hr>
 					<ul>
-						<li>PHONE</li>
-						<li>ADDRESS</li>
-						<li>E-MAIL</li>
-						<li>WEBSITE</li>
+						<li>
+							<ul  class="line-inblock">							
+								<li>电话</li>
+								<li>17707170570/13100695760</li>
+							</ul>
+						</li>
+						<li>
+							<ul class="line-inblock">
+								<li>地址</li>
+								<li>湖北宜昌</li>
+							</ul>
+						</li>
+						<li>
+							<ul class="line-inblock">
+								<li>邮箱</li>
+								<li>308232015@qq.com</li>
+							</ul>
+						</li>
+						<li>
+							<ul class="line-inblock">
+								<li>学校</li>
+								<li>武汉纺织大学</li>
+							</ul>
+						</li>
 					</ul>
 				</div>
 			</div>
@@ -24,6 +57,19 @@
 </script>
 
 <style scoped>
+
+
+	/* 特效文字掉落 */
+
+	.rw-words {
+		overflow: hidden;
+	}
+	.rw-words span {
+		display: inline;
+		white-space: nowrap;
+	}
+
+
 	.home {
 		width: 100%;
 	}
@@ -44,17 +90,50 @@
 		border-radius: 5px;
 	}
 	
+	.scrollText {
+		height: 50%;
+		position: absolute;
+		right: -13%;
+		top: 26%;
+		margin-left: 10rem;
+	}
+	
 	
 
-
 	.intro .line {
-		border:4px double white;
+		border:4px double white; 
 		padding: 1.5rem;
+		font-family: "宋体";
 	}
 
 	.line h3 {
 		color: white;
 	}
+	ul {
+		list-style: none;
+		padding-left: 0;
+	}
+
+	.line-inblock {
+		list-style: none;
+	}
+
+	.line-inblock li {
+		display: inline-block;
+		color: white;
+		margin-right: 5%; 
+	}
+
+
+	/* 图标处理 */
+
+	.icon-phone {
+		width: 50px;
+		height: 50px;
+		background: url(/static/imgs/icons.png) 1542px -1264px;
+	}
+
+
  	@media (max-width: 1366px) {
 		.box {
 			width: 67%;
@@ -75,6 +154,8 @@
 			width: 73%;
 			padding: 1rem;
 		}
+
+
 	}
 
 	@media (max-width: 991px) {
@@ -84,6 +165,9 @@
 		.box {
 			padding: 2rem 0 0;
 		}
+		.scrollText {
+		top: 32%;
+	}
 	}
 	@media (max-width: 667px) {
 		.intro {
@@ -94,6 +178,11 @@
 		.box img {
 			margin-left: 0;
 		}
+
+		.scrollText {
+		top: 33%;
+		right: 24%;
+	}
 	}
 
 
@@ -102,6 +191,11 @@
 			width: 80%;
 			padding: 0;
 		}
+		.scrollText {
+		top: 28%;
+		right: 5%;
+		margin-left:2rem;
+	}
 	}
 
 
