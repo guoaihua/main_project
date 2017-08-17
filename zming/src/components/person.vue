83698d3808664e37363a8c3d641f5a0369102dbd
guoaihua <308232015@qq.com>
2017-08-17 14:22:35 +0800

    :construction:  3d旋转方块

--- zming/src/components/person.vue
+++ zming/src/components/person.vue
@@ -1,12 +1,23 @@
 <template>
 	<div class="person">
-		<h1>about me</h1>
-		<div class="col-md-12">
+		<div class="wrap">
+			<div class="cube">
+				<div class="font out"></div>
+				<div class="back out"></div>
+				<div class="top out"></div>
+				<div class="bottom out"></div>
+				<div class="left out"></div>
+				<div class="right out"></div>
 
+				<div class="font-in in"></div>				
+				<div class="back-in in"></div>
+				<div class="top-in in"></div>
+				<div class="bottom-in in"></div>
+				<div class="left-in in"></div>
+				<div class="right-in in"></div>
+			</div>
 		</div>
-		<div class="clear"></div>
 
-		<div class="clear"></div>
 
 		<span class="winnower-container">	
 				<img src="/static/imgs/winnower.png" alt="" class="img-responsive winnower ">
@@ -32,6 +43,139 @@
 			margin:0 auto;
 /* 			background: white; */
 		}
+
+		/* 旋转方块 */
+		.wrap {
+			margin-top: 3rem;
+			width: 100%;
+			perspective-origin: 50% 50%;
+			perspective: 3000px;
+		}
+
+		.cube {
+			position: relative;
+			margin-left: auto;
+			margin-right: auto;
+			width: 200px;
+			height: 200px;
+			transform-style: preserve-3d;
+		}
+
+		.cube .out {
+			position: absolute;
+			width: 100%;
+			height: 100%;
+		}
+
+		.cube .in {
+			position: absolute;
+			width: 50%;
+			height: 50%;
+			top: 25%;
+			left: 25%;
+
+		}
+
+		.font {
+			background: url('/static/imgs/block1.jpg');
+			transform: translateZ(100px);
+		}
+		.back {
+			background: url('/static/imgs/block2.jpg');
+			transform: translateZ(-100px) rotateY(180deg);
+		}
+		.top {
+			background: url('/static/imgs/block3.jpg');
+			transform: translateY(-100px) rotateX(-90deg);
+			
+		}
+		.bottom {
+			background: url('/static/imgs/block4.jpg');
+			transform: translateY(100px) rotateX(90deg);
+		}
+		.left {
+			background: url('/static/imgs/block5.jpg');
+			transform:  translateX(-100px) rotateY(270deg);
+			
+		}
+		.right {
+			background: url('/static/imgs/block6.jpg');
+			transform: translateX(100px) rotateY(-270deg);
+		
+		}
+
+
+
+
+		.font-in,.back-in,.top-in,.bottom-in,.left-in,.right-in {
+			background: url('/static/imgs/cube.png');
+			background-image:50% 50% 
+		}
+		
+		.font-in {
+			transform: translateZ(50px);
+		}
+
+		.back-in {
+			transform: translateZ(-50px) rotateY(180deg);
+		}
+		.top-in {
+			transform: translateY(-50px) rotateX(-90deg);
+		}
+
+		.bottom-in {
+			transform: translateY(50px) rotateX(90deg);
+		}
+		.left-in {
+			transform: translateX(-50px) rotateY(270deg);
+		}
+
+		.right-in {
+			transform: translateX(50px) rotateY(-270deg);
+		}
+
+		
+
+
+
+		@keyframes spin {
+		    from { transform: rotateX(0deg) rotateY(0deg); }
+		    to { transform: rotateX(360deg) rotateY(360deg); }
+		} 
+		 
+		.cube {
+		    animation: spin 20s infinite linear;
+		}
+
+
+		 .cube:hover .font {
+			transform: translateZ(200px);
+		}
+
+		.cube:hover .back {
+			transform: translateZ(-200px) rotateY(180deg);
+		}
+
+		.cube:hover .top {
+			transform: translateY(-200px) rotateX(-90deg);
+		}
+		.cube:hover .bottom {
+			transform: translateY(200px) rotateX(90deg);
+		}
+		.cube:hover .left {
+			transform: translateX(-200px) rotateY(270deg);
+		}
+		.cube:hover .right{
+			transform: translateX(200px) rotateY(-270deg);
+		}  
+
+
+
+
+
+		/* 旋转方块 */
+
+
 		.clear {
 			clear: both;
 		}
