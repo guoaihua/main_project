ea28dd5f82ad61cbee6e014ef805d8c832b5a228
guoaihua <308232015@qq.com>
2017-09-10 20:28:32 +0800

    :art:  修正错误

--- spider/splier/app.js
+++ spider/splier/app.js
@@ -5,27 +5,29 @@ var cheerio=require('cheerio');
 var fs=require('fs');
 var num=0;
 app.get('/',function(req,res) {
-	// body...
-	 var request = require('request');
-	request('https://movie.douban.com/', function (error, response, body) {
-/* 	 console.log('error:', error); // Print the error if one occurred 
-  	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-  	console.log('body:', body); // Print the HTML for the Google homepage*/
-	$=cheerio.load(body);
- 	var data=[];
-  var ws1 = fs.createWriteStream('./data/message.txt', 'utf-8');
-  $('.ui-slide-content .ui-slide-item').each(function(i,e){
-        //e==this;
+  // body...
+   var request = require('request');
+  request('https://movie.douban.com/', function (error, response, body) {
+   // console.log('error:', error); // Print the error if one occurred
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body); // Print the HTML for the Google homepage
+    $=cheerio.load(body);
+    var data=[];
+     var ws1 = fs.createWriteStream('./data/message.txt', 'utf-8');
+    $('.ui-slide-content .ui-slide-item').each(function(i,e){
         num++;
-  			var obj='obj'+1;
-  			obj={};
-  			obj.title=$(e).attr('data-title');
-  			obj.rate=$(e).find('.subject-rate').text();
-        var mes=num+':'+obj.title+' '+obj.rate+'\n';
+        var obj = {};
+        obj.title=$(e).attr('data-title');
+        obj.rate=$(e).attr('data-rate');
+        obj.img=$(e).find('img').attr('src');
+        if(obj.title!=undefined){
+         var mes=num+':'+obj.title+' '+obj.rate+'\n';
         ws1.write(mes);
-  			data.push(obj);
-  	})
-  	res.send(data);
-	});
+        data.push(obj);
+        }
+    })
+    res.send(data);
+  });
 });
+
 app.listen(3000);
