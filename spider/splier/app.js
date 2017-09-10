585ee6b35f816d93da2569ccc43901a59e910317
guoaihua <308232015@qq.com>
2017-09-10 16:27:39 +0800

    :zap: update

--- spider/splier/app.js
+++ spider/splier/app.js
@@ -2,25 +2,30 @@ var express = require('express');
 var app=express();
 var request=require('request');
 var cheerio=require('cheerio');
-
+var fs=require('fs');
+var num=0;
 app.get('/',function(req,res) {
 	// body...
 	 var request = require('request');
-	request('http://www.iqiyi.com/dianshiju/', function (error, response, body) {
- 	 console.log('error:', error); // Print the error if one occurred 
+	request('https://movie.douban.com/', function (error, response, body) {
+/* 	 console.log('error:', error); // Print the error if one occurred 
   	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-  	console.log('body:', body); // Print the HTML for the Google homepage
-  	$=cheerio.load(body);
+  	console.log('body:', body); // Print the HTML for the Google homepage*/
+	$=cheerio.load(body);
  	var data=[];
-  	$('.site-item_l .site-piclist_pic_link').each(function(i,e){
+  var ws1 = fs.createWriteStream('./data/message.txt', 'utf-8');
+  $('.ui-slide-content .ui-slide-item').each(function(i,e){
+        //e==this;
+        num++;
   			var obj='obj'+1;
   			obj={};
-  			obj.title=$(e).attr('title');
-  			obj.text=$(e).find('.mod-listTitle_right').text() 
+  			obj.title=$(e).attr('data-title');
+  			obj.rate=$(e).find('.subject-rate').text();
+        var mes=num+':'+obj.title+' '+obj.rate+'\n';
+        ws1.write(mes);
   			data.push(obj);
   	})
   	res.send(data);
 	});
 });
-
 app.listen(3000);
